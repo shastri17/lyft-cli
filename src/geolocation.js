1bec9de4b3fa606491fa81c56af5a3e9c0643fb7
Harsha Vamsi <harshavamsi096@gmail.com>
2017-03-19 18:02:09 +0530

    v1.0.0

--- src/geolocation.js
+++ src/geolocation.js
@@ -1,19 +1,19 @@
-var request = require('request')
-var lyft = require('./lyftservice')
+var sync = require('sync-request')
+
 module.exports = {
-    get_coordinates : function(address1, address2, address3, bearer){
-        var data = ""
-        var google_payload = {
-            url : "https://maps.googleapis.com/maps/api/geocode/json?address="+ address1 + "," + address2 + "," + address3 + "&key=" + "AIzaSyCE6oqj_iJCLXjeK9GPp1a5cQ1Gn2HLrJs"
-        }
-        function callback1(error, response, body){
-            info = JSON.parse(body);
-            var lat_1 = info["results"][0]["geometry"]["location"]["lat"]
-            var lng_1 = info["results"][0]["geometry"]["location"]["lng"]
-            lat = parseFloat(lat_1)
-            lng = parseFloat(lng_1)
-            lyft.lyftservice(lat,lng, bearer)
-          }
-        request(google_payload, callback1)
+    get_coordinates : function(address1, address2, address3){
+
+        var res = sync("GET","https://maps.googleapis.com/maps/api/geocode/json?address="+ address1 + "," + address2 + "," + address3 + "&key=" + "AIzaSyCE6oqj_iJCLXjeK9GPp1a5cQ1Gn2HLrJs",{
+
+        });
+        info = JSON.parse(res.getBody());
+        var lat_1 = info["results"][0]["geometry"]["location"]["lat"]
+        var lng_1 = info["results"][0]["geometry"]["location"]["lng"]
+        lat = parseFloat(lat_1)
+        lng = parseFloat(lng_1)
+        var lat_data = lat;
+        var long_data = lng;
+        return [lat_data,long_data]
+
     }
 }
--- src/lyft.js
+++ src/lyft.js
@@ -1,11 +1,44 @@
+'use strict';
+
+/**
+ * Adds commas to a number
+ * @param {number} number
+ * @param {string} locale
+ * @return {string}
+ */
+
 var request = require('request')
 var location = require('./geolocation')
+var lyfttime = require('./lyfttimeservice')
+var lyftprice= require('./lyftpriceservice')
 access_token = ""
 cmd = process.argv[2]
 if(cmd == "time"){
 address = process.argv[3]
 var add = address.split(' ').join('+');
 var address_list = add.split(",")
+}
+else if(cmd == 'price'){
+    if(process.argv[3] == "-s"){
+        if(process.argv[5] == "-e"){
+            start_address = process.argv[4]
+            end_address = process.argv[6]
+            var start_add = start_address.split(' ').join('+');
+            var end_add = end_address.split(' ').join('+');
+            var start_address_list = start_add.split(",")
+            var end_address_list = end_add.split(",")
+        }
+        else {
+            console.log("Usage error")
+        }
+    }
+        else {
+            console.log("Usage error")
+        }
+    }
+
+else{
+    console.log("Usage error")
 }
 const credentials = {
   client: {
@@ -32,7 +65,18 @@ oauth2.clientCredentials.getToken(tokenConfig, (error, result) => {
   token = oauth2.accessToken.create(result);
   access_token = token["token"]["access_token"]
   bearer = "Bearer" + " " + access_token
-if(cmd = "time"){
-location.get_coordinates(address_list[0],address_list[1],address_list[2],bearer)
+if(cmd == "time"){
+var data = location.get_coordinates(address_list[0],address_list[1],address_list[2])
+lyfttime.lyftservice(data[0],data[1],bearer)
+}
+else if(cmd == "price"){
+var start_data = location.get_coordinates(start_address_list[0], start_address_list[1], start_address_list[2])
+var end_data = location.get_coordinates(end_address_list[0], end_address_list[1], end_address_list[2])
+
+
+lyftprice.lyftservice(start_data[0],start_data[1],end_data[0],end_data[1],bearer)
+}
+else{
+    console.log("Usage error")
 }
 });
